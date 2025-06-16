e22bdff9d019776835e733f67a3756f145db348b
Adrien <ascenium@gmail.com>
2025-06-16 15:16:49 +0200

    Ajoutez des fichiers projet.

---
+++ MinimalWebApi/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet(Name = "AutomaticGet")]
+        public string GetBaseString()
+        {
+            return "Hello, World! This is the default controller response.";
+        }
+    }
+}
